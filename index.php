ed39f48adf92e117c607563db4fcb2aca16d719d
azabraao <az.abraao@gmail.com>
2019-12-18 12:23:05 -0300

    Add initial enviroment

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Ibiapaba</title>
+  <link rel="stylesheet" href="./assets/dist/main.min.css">
 </head>
 <body>
-  <h1>Ibiapaba</h1>
+  
 </body>
 </html>
