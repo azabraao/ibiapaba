0e81d87a0fe062208a800f4a8bb211ba4a060940
azabraao <az.abraao@gmail.com>
2019-12-19 16:51:09 -0300

    Add map section

--- index.php
+++ index.php
@@ -281,6 +281,26 @@
     </div>
   </div>
 </section>
+
+<section class="section localizacao no-padding-bottom">
+  <div class="container">
+    <div class="section__header">
+      <div class="section__title">
+        <h2>Localização</h2>
+      </div>
+      <div class="section__subtitle">
+        <p>
+          Veja nossa unidade
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="section__body">
+    <iframe class="localizacao__map"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.142143142862!2d-46.4098828!3d-23.4553367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce625cba9bfe27%3A0xc08afb8feb6aa4de!2sR.%20Lago%20de%20Pedra%2C%201023%20-%20Parque%20Sao%20Miguel%2C%20Guarulhos%20-%20SP%2C%2007260-200!5e0!3m2!1spt-BR!2sbr!4v1576784718823!5m2!1spt-BR!2sbr" 
+    allowfullscreen=""></iframe>
+  </div>
+</section>
 <?php
   include('./components/global_scripts.php');
 ?>
