0c69d4a39011ea40d508399fc80d71e4dd811c22
azabraao <az.abraao@gmail.com>
2019-12-19 11:13:56 -0300

    Configure use of active class

--- components/navbar.php
+++ components/navbar.php
@@ -1,11 +1,3 @@
-
-<!-- 
-
-<?php
-  echo $_SERVER["REQUEST_URI"]
-?>
-
- -->
 <nav class="navbar">
   <div class="navbar__inside">
     <div class="navbar__logo">
@@ -20,22 +12,22 @@
         <span class="line"></span>
       </div>
       <div class="menu__links jsLinks">
-        <a href="/" class="menu__link active">
+        <a href="/" class='menu__link <?php $class = ($url == "/") ?  "active" : ""; echo $class; ?>'>
           <span>Home</span>
         </a>
-        <a href="/empresa" class="menu__link">
+        <a href="/empresa" class='menu__link <?php $class = ($url == "/empresa") ?  "active" : ""; echo $class; ?>'>
           <span>Empresa</span>
         </a>
-        <a href="/produtos" class="menu__link">
+        <a href="/produtos" class='menu__link <?php $class = ($url == "/produtos") ?  "active" : ""; echo $class; ?>'>
           <span>Produtos</span>
         </a>
-        <a href="/servicos" class="menu__link">
+        <a href="/servicos" class='menu__link <?php $class = ($url == "/servicos") ?  "active" : ""; echo $class; ?>'>
           <span>Serviços</span>
         </a>
-        <a href="/historico" class="menu__link">
+        <a href="/historico" class='menu__link <?php $class = ($url == "/historico") ?  "active" : ""; echo $class; ?>'>
           <span>Histórico</span>
         </a>
-        <a href="/contato" class="menu__link">
+        <a href="/contato" class='menu__link <?php $class = ($url == "/contato") ?  "active" : ""; echo $class; ?>'>
           <span>Contato</span>
         </a>
       </div>
