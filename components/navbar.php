48da0faace44cd8247e78351598f7b3165a04391
azabraao <az.abraao@gmail.com>
2019-12-19 10:40:07 -0300

    test

---
+++ components/navbar.php
@@ -0,0 +1,44 @@
+
+<!-- 
+
+<?php
+  echo $_SERVER["REQUEST_URI"]
+?>
+
+ -->
+<nav class="navbar">
+  <div class="navbar__inside">
+    <div class="navbar__logo">
+      <a href="/">
+        <img src="./assets/img/logo.png" alt="Logo Ibiapaba">
+      </a>
+    </div>
+    <div class="menu active">
+      <div class="menu__hamburguer jsHamburguer">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
+      <div class="menu__links jsLinks">
+        <a href="/" class="menu__link active">
+          <span>Home</span>
+        </a>
+        <a href="/empresa" class="menu__link">
+          <span>Empresa</span>
+        </a>
+        <a href="/produtos" class="menu__link">
+          <span>Produtos</span>
+        </a>
+        <a href="/servicos" class="menu__link">
+          <span>Serviços</span>
+        </a>
+        <a href="/historico" class="menu__link">
+          <span>Histórico</span>
+        </a>
+        <a href="/contato" class="menu__link">
+          <span>Contato</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -11,42 +11,9 @@
 </head>
 <body>
 
-<nav class="navbar">
-  <div class="navbar__inside">
-    <div class="navbar__logo">
-      <a href="/">
-        <img src="./assets/img/logo.png" alt="Logo Ibiapaba">
-      </a>
-    </div>
-    <div class="menu active">
-      <div class="menu__hamburguer jsHamburguer">
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
-      </div>
-      <div class="menu__links jsLinks">
-        <a href="/" class="menu__link active">
-          <span>Home</span>
-        </a>
-        <a href="/empresa" class="menu__link">
-          <span>Empresa</span>
-        </a>
-        <a href="/produtos" class="menu__link">
-          <span>Produtos</span>
-        </a>
-        <a href="/servicos" class="menu__link">
-          <span>Serviços</span>
-        </a>
-        <a href="/historico" class="menu__link">
-          <span>Histórico</span>
-        </a>
-        <a href="/contato" class="menu__link">
-          <span>Contato</span>
-        </a>
-      </div>
-    </div>
-  </div>
-</nav>
+<?php 
+  include('./components/navbar.php');
+?>
 
 <header class="header">
   <div class="container">
