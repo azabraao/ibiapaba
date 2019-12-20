e5d0fe69de12faa755177eee25f3ffe7765cbac3
azabraao <az.abraao@gmail.com>
2019-12-20 11:55:44 -0300

    Add content element

--- empresa.php
+++ empresa.php
@@ -18,6 +18,49 @@
 </header>
 
 
+<section class="section">
+  <div class="container">
+    <div class="content-columns">
+      <div class="column content">
+        <h5>
+          Sobre
+        </h5>
+        <p>
+          A Ibiapaba é dedicada para empresas que trabalham com a 
+          produção de mangueiras trançadas, mangueiras para gás, 
+          mangueiras espiraladas, perfis, tubos de pvc, tubos de 
+          látex, tubos de nylon etc. Aqui você encontra a verdadeira 
+          tecnologia para uma produção com maior quantidade e 
+          qualidade.        
+        </p>
+      </div>
+      <div class="column centered">
+        <img src="./assets/img/logo-233x233.png" width="170" alt="Logo ibiapaba">
+      </div>
+    </div>
+    <div class="content-columns">
+      <div class="column centered">
+        <img src="./assets/img/homologado-inmetro.png" alt="Homologado Inmetro">
+      </div>
+      <div class="column content">
+        <h5>
+          Perfil da empresa
+        </h5>
+        <p>
+          Há 20 anos nossa empresa trabalha satisfazendo as 
+          necessidades de empresas de grande, médio e pequeno porte, na 
+          área de ferramentaria, tornearia mecânica e desenvolvimento 
+          de equipamentos que trabalham relacionados com o processo de 
+          extrusão. Trabalhamos também com projetos contínuos em 
+          mangueiras de gás GLP normalizada, seguindo normas do INMETRO,
+           garantimos de 30% à 40% a mais de aderência do que o INMETRO 
+          exige, além de desenvolvermos projetos para cada perfil de 
+          empresa com acessoria total.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
   
 <?php
   include('./components/global_scripts.php');
