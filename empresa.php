09b7f364f4a2c47c66415a6780feb2d0a68a3726
azabraao <az.abraao@gmail.com>
2019-12-20 11:32:12 -0300

    Add page-header component

---
+++ empresa.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php
+  include('./components/global_head.php');
+?>
+<body>
+<?php
+  include('./components/navbar.php');
+?>
+<header class="page-header empresa">
+  <div class="container">
+    <div class="page-header__title">
+      <h1>
+        Seja Bem-vindo á Ibiapaba
+      </h1>  
+    </div>
+  </div>
+</header>
+
+
+  
+<?php
+  include('./components/global_scripts.php');
+?>
+</body>
+</html>
