3e37dfd4af43ae56188713433b0ccbbfce9e565a
Geovanne Amorim Coelho <geovanne.jp@gmail.com>
2023-01-13 23:18:23 -0300

    Verificação Facebook

--- components/global_head.php
+++ components/global_head.php
@@ -2,6 +2,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="facebook-domain-verification" content="mfl4bw1uw0dmf2j6j6vulu815oqc8o" />
   <title>Ibiapaba</title>
   <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon.ico">
   <link rel="stylesheet" href="./assets/dist/main.min.css?cache=2">
